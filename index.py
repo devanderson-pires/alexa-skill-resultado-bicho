4a45f1f3d3ca07d5580e32cc41dbe204dde202d2
Anderson Pires <devanderson.tech@gmail.com>
2022-09-04 21:25:03 -0300

    :recycle: refatorar código

--- index.py
+++ index.py
@@ -1,39 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def resultado(titulo_site, titulo_tabela, horario, bichos):
+    if len(bichos) == 0:
+        return ['O resultado não saiu ainda']
+
     listaDeBichos = list()
     listaDeBichos.append(titulo_site)
     listaDeBichos.append(titulo_tabela)
     listaDeBichos.append(horario)
 
-    count = -1
-    naoTem = ['O', 'resultado', 'não', 'saiu', 'ainda']
-
     for bicho in bichos:
-        count += 1
-
         if bicho.get('title') == '0':
-            bicho['title'] = naoTem[count]
+            return ['O resultado não saiu ainda']
 
-        listaDeBichos.append(bicho.get("title"))
+        listaDeBichos.append(bicho.get('title'))
 
     return listaDeBichos
 
+
 try:
-    url = requests.get("https://www.ojogodobicho.com/deu_no_poste.htm")
-    soup = BeautifulSoup(url.content, "html.parser")
+    url = requests.get('https://www.ojogodobicho.com/deu_no_poste.htm')
+    soup = BeautifulSoup(url.content, 'html.parser')
 
     titulo_site = soup.title.string
-    titulo_tabela = soup.find("caption").string
+    titulo_tabela = soup.find('caption').string
 
-    bichos_manha = soup.select("tbody tr > td:nth-child(2)", limit=5)
-    bichos_tarde = soup.select("tbody tr > td:nth-child(3)", limit=5)
-    bichos_federal = soup.select("tbody tr > td:nth-child(5)", limit=5)
+    bichos_manha = soup.select('tbody tr > td:nth-child(2)', limit=5)
+    bichos_tarde = soup.select('tbody tr > td:nth-child(3)', limit=5)
+    bichos_noite = soup.select('tbody tr > td:nth-child(4)', limit=5)
+    bichos_federal = soup.select('tbody tr > td:nth-child(5)', limit=5)
+    bichos_corujinha = soup.select('tbody tr > td:nth-child(6)', limit=5)
 
-    print(" ".join(resultado(titulo_site, titulo_tabela, 'p.t.m', bichos_manha)))
-    print(" ".join(resultado(titulo_site, titulo_tabela, 'p.t', bichos_tarde)))
-    print(" ".join(resultado(titulo_site, titulo_tabela, 'federal', bichos_federal)))
+    print(' '.join(resultado(titulo_site, titulo_tabela, 'p.t.m', bichos_manha)))
+    print(' '.join(resultado(titulo_site, titulo_tabela, 'p.t', bichos_tarde)))
+    print(' '.join(resultado(titulo_site, titulo_tabela, 'p.t.v', bichos_noite)))
+    print(' '.join(resultado(titulo_site, titulo_tabela, 'federal', bichos_federal)))
+    print(' '.join(resultado(titulo_site, titulo_tabela, 'corujinha', bichos_corujinha)))
 
 except Exception as err:
     print(err)
